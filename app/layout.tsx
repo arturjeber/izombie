8d757f9480491db53000e6b0b94cb3b8e981b2f2
arturjeber <r2jeber@gmail.com>
2025-10-23 18:43:44 -0300

    smnartlook

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Providers from '@/lib/providers';
+import Script from 'next/script';
 
 import '@/styles/main-design.css';
 import './globals.css';
@@ -8,6 +9,24 @@ import './globals.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Script do Smartlook */}
+        <Script
+          id="smartlook"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.smartlook||(function(d) {
+              var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+              var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+              c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';
+              c.onload = function() { smartlook('init', '1d2ce0412ca3552d319fc46a32e798a15ca4f141', { region: 'eu' }); };
+              h.appendChild(c);
+            })(document);
+          `,
+          }}
+        />
+      </head>
       <body>
         {/* Background */}
         <div className="grid-bg"></div>
