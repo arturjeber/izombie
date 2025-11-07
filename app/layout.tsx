8849b78593788e5ac951cfd73e9d846bd1e28b47
arturjeber <r2jeber@gmail.com>
2025-11-07 13:47:29 -0300

    pixel meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,25 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             `,
           }}
         />
+        {/* Meta Pixel base code */}
+        <Script
+          id="facebook-pixel-base"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1488707939079733');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
         <Script id="tiktok-pixel" strategy="afterInteractive">
           {`
             !function (w, d, t) {
